0c2f01dddb75102950a4514ac497362ad6e97d1f
0xChloe <chloe@catastrophesoftware.com>
2025-06-09 21:58:13 +0000

    Update gitignore and migration stuff

--- trakr/migrations/env.py
+++ trakr/migrations/env.py
@@ -3,6 +3,8 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from trakr.db import DATABASE_URL
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -11,7 +13,10 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+if DATABASE_URL.startswith("postgresql+asyncpg"):
+    SYNC_DATABASE_URL = DATABASE_URL.replace("postgresql+asyncpg", "postgresql")
 
+config.set_main_option("sqlalchemy.url", SYNC_DATABASE_URL)
 # add your model's MetaData object here
 # for 'autogenerate' support
 from trakr.models.base import Base
